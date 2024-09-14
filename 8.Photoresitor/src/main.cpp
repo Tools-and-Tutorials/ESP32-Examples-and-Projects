8c367ea0f95b34877902bb2e2e17b327ee19f092
jms <38635781+jeyakumarms@users.noreply.github.com>
2024-09-14 18:27:35 +0100

    Buzz and sample photo added

--- 8.Photoresitor/src/main.cpp
+++ 8.Photoresitor/src/main.cpp
@@ -1,10 +1,10 @@
 #include <Arduino.h>
 
-#define PR1_PIN 15
-#define PR2_PIN 4
+#define PR1_PIN 15  //Must be ADC PIN
+#define BUZZ_PIN 4  //Must be PWM PIN
 
-const int numberOfPins=2;
-int prevValues[] = {0,0};
+
+int prevValue=0;
 
 bool checkChanges(int[]);
 void printValues(int[]);
@@ -12,45 +12,26 @@ void printValues(int[]);
 void setup() {
   Serial.begin(115200);
   pinMode(PR1_PIN, INPUT);
-  pinMode(PR2_PIN, INPUT);
+  pinMode(BUZZ_PIN, OUTPUT);
 }
 
+//Produce short beep sound on light intensity change and long beep on no light
+//In order to reduce the variance ignore last digit of photoresistor value.
 void loop() {
-  int currentValues[] = {0,0}; 
-  currentValues[0]= analogRead(PR1_PIN);
-  currentValues[1]= analogRead(PR2_PIN);
-  if(checkChanges(currentValues))
+  int currentValue = 0; 
+  currentValue= analogRead(PR1_PIN);
+  if(currentValue/10!=prevValue)
   {
-   
-    printValues(currentValues);
-  }
-  delay(100);
-}
-
-//return true if any values changed, false otherwise
-bool checkChanges(int *currentValues)
-{
-  bool changed = false;
-  for (size_t i = 0; i < numberOfPins; i++)
-  {
-    if (currentValues[i]/10 != prevValues[i])
-    {
-      Serial.printf("PR%d: ", i);
-      Serial.println(currentValues[i]);
-      prevValues[i] = currentValues[i]/10;
-      changed = true;
+    prevValue=currentValue/10;
+    Serial.printf("PR1: %d\n", currentValue/10);
+    if(currentValue>0){
+      noTone(BUZZ_PIN);
+      tone(BUZZ_PIN, 1000, 20); //1000 frequency short beep
+    }
+    else{
+      noTone(BUZZ_PIN);
+      tone(BUZZ_PIN, 1000, 500); //1000 frequency long beep
     }
   }
-  return changed;
-}
-
-void printValues(int currentValues[])
-{
-  Serial.printf("Number of items %d",numberOfPins);
-  Serial.println();
-  for (size_t i = 0; i < numberOfPins; i++)
-  {
-    Serial.printf("PR%d: %d ",i,currentValues[i]/10);
-  }
-  Serial.println();
+  delay(100);
 }
